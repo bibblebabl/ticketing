b862ed9d7ccda41726d784877cf30e8bfeeadc0c
Igor Belogurov <belogurovigor@gmail.com>
2022-11-03 23:42:09 +0100

    (auth): adds get currentuser endpoint

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,13 @@ import { json } from "body-parser";
 const app = express();
 app.use(json());
 
+app.get("/api/users/currentuser", (req, res) => {
+  res.send({
+    name: "sdsds",
+    role: "sdsd",
+  });
+});
+
 app.listen(3000, () => {
-  console.log("Listening on port 3000!");
+  console.log("Listening on port 3000!!!!!");
 });
