1b1c452ef1b736eae728d5fce71668939e62bbbc
Igor Belogurov <belogurovigor@gmail.com>
2022-11-06 21:46:36 +0100

    adds signup route to userRouter. moved to local k8s instead gcloud

--- auth/src/routes/users.ts
+++ auth/src/routes/users.ts
@@ -1,4 +1,5 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { body } from "express-validator";
 
 const usersRouter = express.Router();
 
@@ -17,5 +18,25 @@ usersRouter.post("/signout", (req, res) => {
 usersRouter.post("/signup", (req, res) => {
   res.send("Hi there!");
 });
+
+usersRouter.post(
+  "signup",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage("Password must be between 4 and 20 characters"),
+  ],
+  (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    if (!email || typeof email !== "string") {
+      res.status(400).send("Provide a valid email");
+    }
+
+    // new User({ email, password })
+  }
+);
 
 export { usersRouter };
