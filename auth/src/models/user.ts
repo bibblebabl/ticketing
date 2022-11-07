8d808d799fdce9f2235dac58d0e6c6e1c32dacac
Igor Belogurov <belogurovigor@gmail.com>
2022-11-08 00:26:25 +0100

    auth:adds password hashing with bcrypt

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,9 +1,13 @@
-import mongoose, { Document } from 'mongoose'
+import mongoose, { Document, Error } from 'mongoose'
+import bcrypt from 'bcrypt'
+import { BadRequestError } from '../errors'
 
 interface IUser extends Document {
   email: string
   password: string
 }
+
+const HASH_ROUNDS = 10
 
 const userSchema = new mongoose.Schema<IUser>({
   email: {
@@ -14,6 +18,18 @@ const userSchema = new mongoose.Schema<IUser>({
     type: String,
     required: true,
   },
+})
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next()
+  try {
+    const salt = await bcrypt.genSalt(HASH_ROUNDS)
+    const hashed = await bcrypt.hash(this.password, salt)
+    this.set('password', hashed)
+    return next()
+  } catch (error) {
+    throw new BadRequestError('Error with saving User Data')
+  }
 })
 
 const User = mongoose.model<IUser>('User', userSchema)
