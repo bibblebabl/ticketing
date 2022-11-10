73b749c8a6a149bdb5c52cf2e858580613d78eea
Igor Belogurov <belogurovigor@gmail.com>
2022-11-11 00:39:28 +0100

    auth: split index.ts to app.ts

---
+++ auth/src/app.ts
@@ -0,0 +1,29 @@
+import express from 'express'
+import 'express-async-errors'
+import { json } from 'body-parser'
+import cookieSession from 'cookie-session'
+
+import { usersRouter } from './routes/users'
+import { errorHandler } from './middlewares'
+import { NotFoundError } from './errors'
+
+const app = express()
+
+app.set('trust proxy', true) // for ingress-nginx
+app.use(json())
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  }),
+)
+
+app.use('/api/users', usersRouter)
+
+app.all('*', async () => {
+  throw new NotFoundError()
+})
+
+app.use(errorHandler)
+
+export { app }
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,39 +1,14 @@
-import express from 'express'
 import 'express-async-errors'
-import { json } from 'body-parser'
-import cookieSession from 'cookie-session'
 
 import mongoose from 'mongoose'
-
-import { usersRouter } from './routes/users'
-import { errorHandler } from './middlewares'
-import { NotFoundError } from './errors'
+import { app } from './app'
 
 const config = {
   port: 3000,
   mongoDbUri: 'mongodb://auth-mongo-srv:27017/auth',
 }
 
-const app = express()
-
-app.set('trust proxy', true) // for ingress-nginx
-app.use(json())
-app.use(
-  cookieSession({
-    signed: false,
-    secure: true,
-  }),
-)
-
-app.use('/api/users', usersRouter)
-
-app.all('*', async () => {
-  throw new NotFoundError()
-})
-
-app.use(errorHandler)
-
-const init = async () => {
+const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error('process.env.JWT_KEY must be defined')
   }
@@ -50,4 +25,4 @@ const init = async () => {
   })
 }
 
-init()
+start()
