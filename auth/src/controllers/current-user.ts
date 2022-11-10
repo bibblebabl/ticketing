cb1aa9e650262e04b599ae49eae53a9c342a6e2f
Igor Belogurov <belogurovigor@gmail.com>
2022-11-10 23:48:41 +0100

    feat(auth): :sparkles: adds current user controller

--- auth/src/controllers/current-user.ts
+++ auth/src/controllers/current-user.ts
@@ -1,5 +1,17 @@
-import { Request, Response } from "express";
+import { Request, Response } from 'express'
+import jwt from 'jsonwebtoken'
+import { BadRequestError } from '../errors'
 
 export const currentUserController = (req: Request, res: Response) => {
-  res.send("Hi there!");
-};
+  if (!req.session?.jwt) {
+    return res.send({ currentUser: null })
+  }
+
+  try {
+    const userPayload = jwt.verify(req.session.jwt, process.env.JWT_KEY!)
+
+    res.send({ currentUser: userPayload })
+  } catch (error) {
+    res.send({ currentUser: null })
+  }
+}
