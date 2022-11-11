87411af1ec683ac524f3cd3303e08c25dfd609f2
Igor Belogurov <belogurovigor@gmail.com>
2022-11-11 23:49:06 +0100

    auth: sign-up.test - duplicate emails

--- auth/src/controllers/__test__/sign-up.test.ts
+++ auth/src/controllers/__test__/sign-up.test.ts
@@ -35,3 +35,21 @@ it('returns a 400 with missing email and password', async () => {
   await request(app).post('/api/users/signup').send({ email: 'test@test.com' }).expect(400)
   await request(app).post('/api/users/signup').send({ password: 'sdsdsjdskjd' }).expect(400)
 })
+
+it('disallows duplicate emails', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'passowrd',
+    })
+    .expect(201)
+
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'passowrd',
+    })
+    .expect(400)
+})
