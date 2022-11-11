45f75545080c597610de281217a96f97dd2907da
Igor Belogurov <belogurovigor@gmail.com>
2022-11-11 23:47:08 +0100

    auth: adds basic tets for sign-up controller

--- auth/src/controllers/__test__/sign-up.test.ts
+++ auth/src/controllers/__test__/sign-up.test.ts
@@ -10,3 +10,28 @@ it('returns a 201 on a successful signup', async () => {
     })
     .expect(201)
 })
+
+it('returns a 400 with invalid email', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'skfjddkjfd',
+      password: 'password',
+    })
+    .expect(400)
+})
+
+it('returns a 400 with invalid password', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'p',
+    })
+    .expect(400)
+})
+
+it('returns a 400 with missing email and password', async () => {
+  await request(app).post('/api/users/signup').send({ email: 'test@test.com' }).expect(400)
+  await request(app).post('/api/users/signup').send({ password: 'sdsdsjdskjd' }).expect(400)
+})
