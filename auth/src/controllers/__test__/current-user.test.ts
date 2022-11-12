3ad2964bf3e94fb718f67d29140ad217b9017204
Igor Belogurov <belogurovigor@gmail.com>
2022-11-12 23:52:50 +0100

    auth: change name of the test

--- auth/src/controllers/__test__/current-user.test.ts
+++ auth/src/controllers/__test__/current-user.test.ts
@@ -13,7 +13,7 @@ it('response with details about current user', async () => {
   expect(response.body.currentUser.email).toEqual('test@test.com')
 })
 
-it('responds with null if not authenticated', async () => {
+it('responds with undefined if not authenticated', async () => {
   const response = await request(app).get('/api/users/currentuser').send().expect(401)
 
   expect(response.body.currentUser).toEqual(undefined)
