f46b79879c92d9343e619c1029426cb006ebe3d2
Igor Belogurov <belogurovigor@gmail.com>
2022-11-13 22:50:16 +0100

    client: adds request for signup via axios

--- client/pages/auth/signup.tsx
+++ client/pages/auth/signup.tsx
@@ -1,6 +1,23 @@
 import Head from 'next/head'
+import { useState } from 'react'
+import axios from 'axios'
 
 export default function SignUp() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    console.log(email, password)
+
+    const response = await axios.post('/api/users/signup', {
+      email,
+      password,
+    })
+
+    console.log(response)
+  }
+
   return (
     <div>
       <Head>
@@ -10,24 +27,28 @@ export default function SignUp() {
       </Head>
 
       <main>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="form-group">
             <label htmlFor="exampleInputEmail1">Email address</label>
             <input
+              value={email}
               type="email"
               className="form-control"
               id="exampleInputEmail1"
               aria-describedby="emailHelp"
               placeholder="Enter email"
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
           <div className="form-group">
             <label htmlFor="exampleInputPassword1">Password</label>
             <input
+              value={password}
               type="password"
               className="form-control"
               id="exampleInputPassword1"
               placeholder="Password"
+              onChange={(e) => setPassword(e.target.value)}
             />
           </div>
           <button type="submit" className="btn btn-primary">
