ceb1032135bc8ddd909735bc2693cfdd7f5c2a70
Igor Belogurov <belogurovigor@gmail.com>
2023-01-30 21:30:47 +0100

    adds tickets back

---
+++ apps/tickets/src/index.ts
@@ -0,0 +1,33 @@
+import 'express-async-errors'
+import dotEnvSafe from 'dotenv-safe'
+
+dotEnvSafe.config({
+  example: './.env.example',
+})
+
+import mongoose from 'mongoose'
+import { app } from './app'
+
+const config = {
+  port: 3000,
+  mongoDbUri: 'mongodb://auth-mongo-srv:27017/auth',
+}
+
+const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error('process.env.JWT_KEY must be defined')
+  }
+
+  try {
+    await mongoose.connect(config.mongoDbUri)
+    console.log('Connected to MongoDB')
+  } catch (error) {
+    console.error(error)
+  }
+
+  app.listen(config.port, () => {
+    console.log(`Auth Server listening on ${config.port}`)
+  })
+}
+
+start()
