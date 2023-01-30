fbbe1f0529c547ee2f5196735fa98f4766986ef8
Igor Belogurov <belogurovigor@gmail.com>
2023-01-30 21:08:04 +0100

    adds tickets app

---
+++ apps/tickets/src/app.ts
@@ -0,0 +1,24 @@
+import express from 'express'
+import 'express-async-errors'
+import { json } from 'body-parser'
+import cookieSession from 'cookie-session'
+import { errorHandler, NotFoundError } from '@bibblebabl/common'
+
+const app = express()
+
+app.set('trust proxy', true) // for ingress-nginx
+app.use(json())
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test',
+  }),
+)
+
+app.all('*', async () => {
+  throw new NotFoundError()
+})
+
+app.use(errorHandler)
+
+export { app }
