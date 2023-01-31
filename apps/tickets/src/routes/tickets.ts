0ce473de95d15691417032b25adf0e9e677526f9
Igor Belogurov <belogurovigor@gmail.com>
2023-01-31 22:36:35 +0100

    adds get tickets route

--- apps/tickets/src/routes/tickets.ts
+++ apps/tickets/src/routes/tickets.ts
@@ -1,12 +1,15 @@
 import { requireAuth, validateRequest } from '@bibblebabl/common'
 import express from 'express'
+import { getTicketsController } from '../controllers/get-tickets'
 import { newTicketController, ticketValidator } from '../controllers/new-ticket'
 import { showTicketController } from '../controllers/show-ticket'
 
 const ticketsRouter = express.Router()
 
-ticketsRouter.post('/', requireAuth, ticketValidator, validateRequest, newTicketController)
+ticketsRouter.get('/', getTicketsController)
 
 ticketsRouter.get('/:id', showTicketController)
+
+ticketsRouter.post('/', requireAuth, ticketValidator, validateRequest, newTicketController)
 
 export { ticketsRouter }
