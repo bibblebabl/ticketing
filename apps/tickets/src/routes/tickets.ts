227842143d047410fafbc285f763684cd1dbc961
Igor Belogurov <belogurovigor@gmail.com>
2023-01-31 10:35:37 +0100

    adds title and price validation

--- apps/tickets/src/routes/tickets.ts
+++ apps/tickets/src/routes/tickets.ts
@@ -1,10 +1,20 @@
-import { requireAuth } from '@bibblebabl/common'
-import express from 'express'
+import { requireAuth, validateRequest } from '@bibblebabl/common'
+import { body } from 'express-validator'
+import express, { Response, Request } from 'express'
 
 const ticketsRouter = express.Router()
 
-ticketsRouter.post('/', requireAuth, (req, res) => {
-  res.status(200).send({})
-})
+ticketsRouter.post(
+  '/',
+  requireAuth,
+  [
+    body('title').not().isEmpty().withMessage('Title is required'),
+    body('price').isFloat({ gt: 0 }).withMessage('Price must be greater than 0'),
+  ],
+  validateRequest,
+  (req: Request, res: Response) => {
+    res.status(200).send({})
+  },
+)
 
 export { ticketsRouter }
