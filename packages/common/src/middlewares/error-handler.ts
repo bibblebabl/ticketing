a92313ce08daeac15dacb71724315159d126acd9
Igor Belogurov <belogurovigor@gmail.com>
2023-01-31 22:23:45 +0100

    adds console log to errorHandler middleware

--- packages/common/src/middlewares/error-handler.ts
+++ packages/common/src/middlewares/error-handler.ts
@@ -1,17 +1,14 @@
-import { Request, Response, NextFunction } from "express";
-import { CustomError } from "../errors/custom-error";
+import { Request, Response, NextFunction } from 'express'
+import { CustomError } from '../errors/custom-error'
 
-export const errorHandler = (
-  err: Error,
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
+export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
   if (err instanceof CustomError) {
-    return res.status(err.statusCode).send({ errors: err.serializeErrors() });
+    return res.status(err.statusCode).send({ errors: err.serializeErrors() })
   }
 
+  console.error(err)
+
   res.status(400).send({
-    errors: [{ message: "Something went wrong" }],
-  });
-};
+    errors: [{ message: 'Something went wrong' }],
+  })
+}
