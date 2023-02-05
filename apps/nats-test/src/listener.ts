6aa7bda8df8581e6640687cd68835e12b0fd6ef3
Igor Belogurov <belogurovigor@gmail.com>
2023-02-05 22:07:38 +0100

    adds listener and publisher

---
+++ apps/nats-test/src/listener.ts
@@ -0,0 +1,19 @@
+import nats from 'node-nats-streaming'
+
+console.clear()
+
+const stan = nats.connect('ticketing', 'cba', {
+  url: 'http://localhost:4222',
+})
+
+stan.on('connect', () => {
+  console.log('Listener connected to NATS')
+
+  const subscription = stan.subscribe('ticket:created')
+
+  subscription.on('message', (msg) => {
+    console.log('Message received from NATS!')
+
+    // console.log(msg)
+  })
+})
