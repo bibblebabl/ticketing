0ddaa08dcef93cabe35bc6885ae1487b5e449f72
Igor Belogurov <belogurovigor@gmail.com>
2023-02-05 22:46:05 +0100

    adds clientId to nats listener

--- apps/nats-test/src/listener.ts
+++ apps/nats-test/src/listener.ts
@@ -1,13 +1,16 @@
 import nats, { Message } from 'node-nats-streaming'
+import { randomBytes } from 'crypto'
 
 console.clear()
 
-const stan = nats.connect('ticketing', 'cba', {
+const clientID = randomBytes(4).toString('hex')
+
+const stan = nats.connect('ticketing', clientID, {
   url: 'http://localhost:4222',
 })
 
 stan.on('connect', () => {
-  console.log('Listener connected to NATS')
+  console.log('Listener connected to NATS with client ID: ', clientID)
 
   const subscription = stan.subscribe('ticket:created')
 
