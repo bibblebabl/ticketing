798e89fd8109deaaedc4c613a92860761612606d
Igor Belogurov <belogurovigor@gmail.com>
2023-02-07 19:24:09 +0100

    adds listener classes

--- apps/nats-test/src/listener.ts
+++ apps/nats-test/src/listener.ts
@@ -1,4 +1,4 @@
-import nats, { Message } from 'node-nats-streaming'
+import nats, { Message, Stan } from 'node-nats-streaming'
 import { randomBytes } from 'crypto'
 
 console.clear()
@@ -17,26 +17,66 @@ stan.on('connect', () => {
     process.exit()
   })
 
-  const options = stan
-    .subscriptionOptions()
-    .setDeliverAllAvailable()
-    .setDurableName('accounting-service')
-    .setManualAckMode(true)
-
-  // .setManualAckMode(true).setMaxInFlight(1).setDeliverAllAvailable().setDurableName('orders-service')
-
-  const subscription = stan.subscribe('ticket:created', 'queue-group-name', options)
-
-  subscription.on('message', (msg: Message) => {
-    const data = msg.getData()
-
-    if (typeof data === 'string') {
-      console.log(`Received event #${msg.getSequence()}, with data: ${data}`)
-    }
-
-    msg.ack()
-  })
+  new TicketCreatedListener(stan).listen()
 })
 
 process.on('SIGINT', () => stan.close())
 process.on('SIGTERM', () => stan.close())
+
+abstract class Listener {
+  abstract subject: string
+  abstract queueGroupName: string
+  abstract onMessage(data: any, msg: Message): void
+  private client: Stan
+  protected ackWait = 5 * 1000
+
+  constructor(client: Stan) {
+    this.client = client
+  }
+
+  subscriptionOptions() {
+    return this.client
+      .subscriptionOptions()
+      .setDeliverAllAvailable()
+      .setManualAckMode(true)
+      .setAckWait(this.ackWait)
+      .setDurableName(this.queueGroupName)
+  }
+
+  listen() {
+    const subscription = this.client.subscribe(
+      this.subject,
+      this.queueGroupName,
+      this.subscriptionOptions(),
+    )
+
+    subscription.on('message', (msg: Message) => {
+      console.log(`Message received: ${this.subject} / ${this.queueGroupName}`)
+
+      const parsedData = this.parseMessage(msg)
+
+      this.onMessage(parsedData, msg)
+    })
+  }
+
+  parseMessage(msg: Message) {
+    const data = msg.getData()
+
+    return typeof data === 'string' ? JSON.parse(data) : JSON.parse(data.toString('utf8'))
+  }
+
+  // onMessage(data: any, msg: Message): void {
+  //   throw new Error('Method not implemented.')
+  // }
+}
+
+class TicketCreatedListener extends Listener {
+  subject = 'ticket:created'
+  queueGroupName = 'payments-service'
+
+  onMessage(data: any, msg: Message) {
+    console.log('Event data!', data)
+
+    msg.ack()
+  }
+}
