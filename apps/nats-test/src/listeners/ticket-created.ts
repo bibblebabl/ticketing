750a92e3f5c777035d0abf577c3a1706ced63b96
Igor Belogurov <belogurovigor@gmail.com>
2023-02-07 20:11:13 +0100

    adds readonly instead of specifying the type for ticker createrd listener

--- apps/nats-test/src/listeners/ticket-created.ts
+++ apps/nats-test/src/listeners/ticket-created.ts
@@ -12,7 +12,7 @@ export interface TicketCreatedEvent {
 }
 
 export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
-  subject: Subjects.TicketCreated = Subjects.TicketCreated
+  readonly subject = Subjects.TicketCreated
   queueGroupName = 'payments-service'
 
   onMessage(data: TicketCreatedEvent['data'], msg: Message) {
