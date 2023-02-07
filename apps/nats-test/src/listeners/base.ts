4d273ebfc82c5919c530288fbb3f9b2d4a7c4ede
Igor Belogurov <belogurovigor@gmail.com>
2023-02-07 20:48:58 +0100

    improves console log of base listener

--- apps/nats-test/src/listeners/base.ts
+++ apps/nats-test/src/listeners/base.ts
@@ -34,7 +34,7 @@ export abstract class Listener<T extends Event> {
     )
 
     subscription.on('message', (msg: Message) => {
-      console.log(`Message received: ${this.subject} / ${this.queueGroupName}`)
+      console.log(`Received event #${msg.getSequence()}, ${this.subject} / ${this.queueGroupName}`)
 
       const parsedData = this.parseMessage(msg)
 
