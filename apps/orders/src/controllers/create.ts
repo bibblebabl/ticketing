834d1f9f99364d1e5cd1cf9b413544705b2b69c3
Igor Belogurov <belogurovigor@gmail.com>
2023-02-26 11:00:07 +0100

    adds tickets validator

--- apps/orders/src/controllers/create.ts
+++ apps/orders/src/controllers/create.ts
@@ -1,4 +1,14 @@
 import { Response, Request } from 'express'
+import { body } from 'express-validator'
+import mongoose from 'mongoose'
+
+export const ticketValidator = [
+  body('ticketId')
+    .not()
+    .isEmpty()
+    .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+    .withMessage('Ticket must be provided'),
+]
 
 export const createOrderController = async (req: Request, res: Response) => {
   // const tickets = await Ticket.find({})
--- apps/orders/src/routes/index.ts
+++ apps/orders/src/routes/index.ts
@@ -1,5 +1,6 @@
+import { requireAuth } from '@bibblebabl/common'
 import express from 'express'
-import { createOrderController } from '../controllers/create'
+import { createOrderController, ticketValidator } from '../controllers/create'
 import { deleteOrderController } from '../controllers/delete'
 import { getOrderController } from '../controllers/get'
 import { getOrdersController } from '../controllers/get-all'
@@ -7,8 +8,11 @@ import { getOrdersController } from '../controllers/get-all'
 const ordersRouter = express.Router()
 
 ordersRouter.get('/', getOrdersController)
+
+ordersRouter.post('/', requireAuth, ticketValidator, createOrderController)
+
 ordersRouter.delete('/:orderId', deleteOrderController)
+
 ordersRouter.get('/:orderId', getOrderController)
-ordersRouter.post('/', createOrderController)
 
 export { ordersRouter }
