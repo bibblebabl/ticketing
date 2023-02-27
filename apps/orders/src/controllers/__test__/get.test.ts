b46f668069ab280660fbcff40ac5f36b81e39a96
Igor Belogurov <belogurovigor@gmail.com>
2023-02-27 18:15:01 +0100

    test(orders): :white_check_mark: adds get order tests

---
+++ apps/orders/src/controllers/__test__/get.test.ts
@@ -0,0 +1,49 @@
+import request from 'supertest'
+import { signIn } from '../../../test/helpers'
+import { apIRoute, app } from '../../app'
+import { Ticket } from '../../models/ticket'
+
+it('fetches the order', async () => {
+  const ticket = new Ticket({
+    title: 'concert',
+    price: 20,
+  })
+
+  await ticket.save()
+
+  const user = signIn()
+
+  const { body: order } = await request(app)
+    .post(apIRoute)
+    .set('Cookie', user)
+    .send({ ticketId: ticket.id })
+    .expect(201)
+
+  const { body: fetchedOrder } = await request(app)
+    .get(`${apIRoute}/${order.id}`)
+    .set('Cookie', user)
+    .expect(200)
+
+  expect(fetchedOrder.id).toEqual(order.id)
+})
+
+it('returns an error if one user tries to fetch another users order', async () => {
+  const ticket = new Ticket({
+    title: 'concert',
+    price: 20,
+  })
+
+  await ticket.save()
+
+  const user = signIn()
+
+  const { body: order } = await request(app)
+    .post(apIRoute)
+    .set('Cookie', user)
+    .send({ ticketId: ticket.id })
+    .expect(201)
+
+  const anotherUser = signIn()
+
+  await request(app).get(`${apIRoute}/${order.id}`).set('Cookie', anotherUser).expect(401)
+})
