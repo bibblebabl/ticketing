f53a2a3a9b2a2c74216f8f0306dc650b2f96d264
Igor Belogurov <belogurovigor@gmail.com>
2023-02-27 19:43:04 +0100

    feat(orders): adds order events

--- packages/common/src/nats/index.ts
+++ packages/common/src/nats/index.ts
@@ -1,7 +1,7 @@
 export { Listener } from './base-listener'
-export { TicketCreatedEvent } from './events/ticket-created'
-export { TicketUpdatedEvent } from './events/ticket-updated'
 
 export { Publisher } from './base-publisher'
 
 export { Subjects } from './subjects'
+
+export * from './events'
