531b9ff160126828cff5811dee6af4cdd3b6de29
Igor Belogurov <belogurovigor@gmail.com>
2023-02-27 16:06:55 +0100

    adds create order is already reserved ticket

--- apps/orders/src/controllers/__test__/create.test.ts
+++ apps/orders/src/controllers/__test__/create.test.ts
@@ -1,12 +1,40 @@
-import { apIRoute, app } from '../../app'
+import { OrderStatus } from '@bibblebabl/common'
 import request from 'supertest'
+import { apIRoute, app } from '../../app'
 import { createMongooseId, signIn } from '../../../test/helpers'
+import { Ticket } from '../../models/ticket'
+import { Order } from '../../models/order'
 
 it('returns an error if the ticket does not exist', async () => {
   const ticketId = createMongooseId()
   await request(app).post(apIRoute).set('Cookie', signIn()).send({ ticketId }).expect(404)
 })
 
-it('returns an error if the ticket is already reserved', async () => {})
+it('returns an error if the ticket is already reserved', async () => {
+  const ticketId = createMongooseId()
+
+  const ticket = new Ticket({
+    id: ticketId,
+    title: 'concert',
+    price: 20,
+  })
+
+  await ticket.save()
+
+  const order = new Order({
+    ticket,
+    userId: '123',
+    status: OrderStatus.Created,
+    expiresAt: new Date(),
+  })
+
+  await order.save()
+
+  await request(app)
+    .post(apIRoute)
+    .set('Cookie', signIn())
+    .send({ ticketId: ticket.id })
+    .expect(400)
+})
 
 it('reserves a ticket', async () => {})
