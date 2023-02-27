9d15ec3f1964ac036c563aa63edddf6df7fb92a5
Igor Belogurov <belogurovigor@gmail.com>
2023-02-27 14:21:52 +0100

    updates create controller for order to set expiration date

--- apps/orders/src/controllers/create.ts
+++ apps/orders/src/controllers/create.ts
@@ -5,6 +5,8 @@ import mongoose from 'mongoose'
 import { Order } from '../models/order'
 import { Ticket } from '../models/ticket'
 
+const EXPIRATION_WINDOW_SECONDS = 15 * 60 * 1000 // 15 minutes
+
 export const ticketValidator = [
   body('ticketId')
     .not()
@@ -23,23 +25,24 @@ export const createOrderController = async (req: Request, res: Response) => {
   }
 
   // Make sure that this ticket is not already reserved
-  // Run query to look at all orders. Find an order where the ticket
-  // is the ticket we just found *and* the orders status is *not* cancelled.
-  // If we find an order from that means the ticket *is* reserved
+  const isReserved = await ticket.isReserved()
 
-  // const isReserved = await ticket.isReserved()
-  const existingOrder = await Order.findOne({
-    ticket,
-    status: {
-      $in: [OrderStatus.Created, OrderStatus.AwaitingPayment, OrderStatus.Complete],
-    },
-  })
-
-  if (existingOrder) {
+  if (isReserved) {
     throw new BadRequestError('Ticket is already reserved')
   }
 
   // Calculate an expiration date for this order
   const expiration = new Date()
-  expiration.setSeconds(expiration.getSeconds() + 15 * 60)
+  expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS)
+
+  const order = new Order({
+    userId: req.currentUser!.id,
+    status: OrderStatus.Created,
+    expiresAt: expiration,
+    ticket,
+  })
+
+  await order.save()
+
+  res.status(201).send(order)
 }
