49ccdcd01ff810abe688eec79704d03d8f8f15b9
Igor Belogurov <belogurovigor@gmail.com>
2023-02-28 17:15:08 +0100

    refactored subject assignment to readonly

--- apps/orders/src/events/publishers/order-cancelled.ts
+++ apps/orders/src/events/publishers/order-cancelled.ts
@@ -1,5 +1,5 @@
 import { Publisher, OrderCreatedEvent, Subjects, OrderCancelledEvent } from '@bibblebabl/common'
 
 export class OrderCancelledPublisher extends Publisher<OrderCancelledEvent> {
-  subject: Subjects.OrderCancelled = Subjects.OrderCancelled
+  readonly subject = Subjects.OrderCancelled
 }
