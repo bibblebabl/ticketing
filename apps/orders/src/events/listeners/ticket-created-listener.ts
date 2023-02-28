0473f2cd1a4445665d90df903bfa3931abf77c35
Igor Belogurov <belogurovigor@gmail.com>
2023-02-28 13:15:16 +0100

    feat(orders): :sparkles: 395
    adds Ticket Created Listener

---
+++ apps/orders/src/events/listeners/ticket-created-listener.ts
@@ -0,0 +1,23 @@
+import { Message } from 'node-nats-streaming'
+import { Listener, Subjects, TicketCreatedEvent } from '@bibblebabl/common'
+import { QUEUE_GROUP_NAME } from '../../config'
+import { Ticket } from '../../models/ticket'
+
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+  readonly subject = Subjects.TicketCreated
+  queueGroupName = QUEUE_GROUP_NAME
+
+  async onMessage(data: TicketCreatedEvent['data'], msg: Message) {
+    const { id, title, price } = data
+
+    const ticket = new Ticket({
+      id,
+      title,
+      price,
+    })
+
+    await ticket.save()
+
+    msg.ack()
+  }
+}
