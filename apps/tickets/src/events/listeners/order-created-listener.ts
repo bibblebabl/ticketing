5fc0e07e5f6de51591d2d54015663dffbdf437b7
Igor Belogurov <belogurovigor@gmail.com>
2023-04-04 17:58:02 +0200

    publishing the event of ticket updated on order created listener

--- apps/tickets/src/events/listeners/order-created-listener.ts
+++ apps/tickets/src/events/listeners/order-created-listener.ts
@@ -1,6 +1,7 @@
 import { Listener, OrderCreatedEvent, Subjects } from '@bibblebabl/common'
 import { Message } from 'node-nats-streaming'
 import { Ticket } from '../../models/ticket'
+import { TicketUpdatedPublisher } from '../publishers/ticket-updated-publisher'
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   subject: Subjects.OrderCreated = Subjects.OrderCreated
@@ -17,6 +18,15 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
 
     // Mark the ticket as being reserved by setting its orderId property
     ticket.set({ orderId: data.id })
+
+    new TicketUpdatedPublisher(this.client).publish({
+      id: ticket.id,
+      version: ticket.version,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+      orderId: ticket.orderId,
+    })
 
     // Save the ticket
     await ticket.save()
