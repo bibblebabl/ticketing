6ab9da9e9aa85daf323d3017076e7ef38e16a8c0
Igor Belogurov <belogurovigor@gmail.com>
2023-04-04 14:04:00 +0200

    adds order created listener

---
+++ apps/tickets/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,27 @@
+import { Listener, OrderCreatedEvent, Subjects } from '@bibblebabl/common'
+import { Message } from 'node-nats-streaming'
+import { Ticket } from '../../models/ticket'
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  subject: Subjects.OrderCreated = Subjects.OrderCreated
+  queueGroupName = 'tickets-service'
+
+  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+    // Find the ticket that the order is reserving
+    const ticket = await Ticket.findById(data.ticket.id)
+
+    // If no ticket, throw error
+    if (!ticket) {
+      throw new Error('Ticket not found')
+    }
+
+    // Mark the ticket as being reserved by setting its orderId property
+    ticket.set({ orderId: data.id })
+
+    // Save the ticket
+    await ticket.save()
+
+    // Ack the message
+    msg.ack()
+  }
+}
