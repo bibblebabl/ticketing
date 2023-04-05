2b8da6a50c061be8aa34173a1676513d7f5916fa
Igor Belogurov <belogurovigor@gmail.com>
2023-04-05 10:36:02 +0200

    feat(expiration): adds initial setup files

---
+++ apps/expiration/src/config.ts
@@ -0,0 +1,7 @@
+import { cleanEnv, str } from 'envalid'
+
+export const env = cleanEnv(process.env, {
+  NATS_CLUSTER_ID: str(),
+  NATS_CLIENT_ID: str(),
+  NATS_URL: str(),
+})
---
+++ apps/expiration/src/index.ts
@@ -0,0 +1,21 @@
+import 'express-async-errors'
+import { env } from './config'
+import { natsWrapper } from './nats-wrapper'
+
+const start = async () => {
+  try {
+    await natsWrapper.connect(env.NATS_CLUSTER_ID, env.NATS_CLIENT_ID, env.NATS_URL)
+
+    natsWrapper.client.on('close', () => {
+      console.log('NATS connection closed!')
+      process.exit()
+    })
+
+    process.on('SIGINT', () => natsWrapper.client.close())
+    process.on('SIGTERM', () => natsWrapper.client.close())
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+start()
