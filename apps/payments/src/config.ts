6ac9a5f238139953ba50c98de784eafaaa9750ca
Igor Belogurov <belogurovigor@gmail.com>
2023-04-13 20:03:36 +0200

    feat(payments): adds stripe instance and adds it to create controller

--- apps/payments/src/config.ts
+++ apps/payments/src/config.ts
@@ -9,4 +9,5 @@ export const env = cleanEnv(process.env, {
   NATS_CLUSTER_ID: str(),
   NATS_CLIENT_ID: str(),
   NATS_URL: str(),
+  STRIPE_SECRET_KEY: str(),
 })
--- apps/payments/src/controller/create.ts
+++ apps/payments/src/controller/create.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express'
 import { body } from 'express-validator'
 import { Order } from '../models/order'
 import { BadRequestError, NotAuthorizedError, NotFoundError, OrderStatus } from '@bibblebabl/common'
+import { stripe } from '../stripe'
 
 export const createPaymentValidator = [
   body('token').not().isEmpty(),
@@ -24,6 +25,12 @@ export const createPaymentController = async (req: Request, res: Response) => {
   if (order.status === OrderStatus.Cancelled) {
     throw new BadRequestError('Cannot pay for an cancelled order')
   }
+
+  await stripe.charges.create({
+    currency: 'usd',
+    amount: order.price * 100,
+    source: token,
+  })
 
   res.status(201).send({ success: true })
 }
