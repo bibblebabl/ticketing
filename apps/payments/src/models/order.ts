dcbf7377842cc39cb16b881fa92e76c83f1e49b3
Igor Belogurov <belogurovigor@gmail.com>
2023-04-13 12:14:47 +0200

    chore:(payments) removes unused packages

--- apps/payments/src/models/order.ts
+++ apps/payments/src/models/order.ts
@@ -1,31 +1,64 @@
 import { OrderStatus } from '@bibblebabl/common'
-import { Model, Document, Schema } from 'mongoose'
+import { model, Model, Document, Schema } from 'mongoose'
+import { updateIfCurrentPlugin } from 'mongoose-update-if-current'
 
 interface OrderAttrs {
   id: string
-  status: OrderStatus
-  userId: string
   version: number
+  userId: string
   price: number
+  status: OrderStatus
 }
 
-interface OrderDoc extends Document {}
+interface OrderDoc extends Document {
+  version: number
+  userId: string
+  price: number
+  status: OrderStatus
+}
 
-interface OrderModel extends Model<OrderDoc> {}
+interface OrderModel extends Model<OrderDoc> {
+  build(attrs: OrderAttrs): OrderDoc
+}
 
-const orderSchema = new Schema({
-  userId: {
-    type: String,
-    required: true,
+const orderSchema = new Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: Object.values(OrderStatus),
+      default: OrderStatus.Created,
+    },
   },
-  price: {
-    type: Number,
-    required: true,
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id
+        delete ret._id
+      },
+    },
   },
-  status: {
-    type: String,
-    required: true,
-    enum: Object.values(OrderStatus),
-    default: OrderStatus.Created,
-  },
-})
+)
+
+orderSchema.set('versionKey', 'version')
+orderSchema.plugin(updateIfCurrentPlugin)
+
+orderSchema.statics.build = (attrs: OrderAttrs) => {
+  return new Order({
+    _id: attrs.id,
+    version: attrs.version,
+    price: attrs.price,
+    userId: attrs.userId,
+    status: attrs.status,
+  })
+}
+
+const Order = model<OrderDoc, OrderModel>('Order', orderSchema)
