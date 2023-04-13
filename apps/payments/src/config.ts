6c2b0e69b6a3459b606a6b860bf68a1f73e4f216
Igor Belogurov <belogurovigor@gmail.com>
2023-04-13 20:14:07 +0200

    renames STRIPE_SECRET_KEY env to STRIPE_KEY

--- apps/payments/src/config.ts
+++ apps/payments/src/config.ts
@@ -9,5 +9,5 @@ export const env = cleanEnv(process.env, {
   NATS_CLUSTER_ID: str(),
   NATS_CLIENT_ID: str(),
   NATS_URL: str(),
-  STRIPE_SECRET_KEY: str(),
+  STRIPE_KEY: str(),
 })
--- apps/payments/src/stripe.ts
+++ apps/payments/src/stripe.ts
@@ -1,6 +1,6 @@
 import Stripe from 'stripe'
 import { env } from './config'
 
-export const stripe = new Stripe(env.STRIPE_SECRET_KEY, {
+export const stripe = new Stripe(env.STRIPE_KEY, {
   apiVersion: '2022-11-15',
 })
