644808640bce7ee922975f7c78829258c8265985
Igor Belogurov <belogurovigor@gmail.com>
2023-04-13 22:03:44 +0200

    payment: emitting payment created event

--- apps/payments/src/controller/create.ts
+++ apps/payments/src/controller/create.ts
@@ -4,6 +4,8 @@ import { Order } from '../models/order'
 import { BadRequestError, NotAuthorizedError, NotFoundError, OrderStatus } from '@bibblebabl/common'
 import { stripe } from '../stripe'
 import { Payment } from '../models/payment'
+import { PaymentCreatedPublisher } from '../events/publishers/payment-created-publisher'
+import { natsWrapper } from '../nats-wrapper'
 
 export const createPaymentValidator = [
   body('token').not().isEmpty(),
@@ -40,5 +42,11 @@ export const createPaymentController = async (req: Request, res: Response) => {
 
   await payment.save()
 
-  res.status(201).send({ success: true })
+  await new PaymentCreatedPublisher(natsWrapper.client).publish({
+    id: payment.id,
+    orderId: payment.orderId,
+    stripeId: payment.stripeId,
+  })
+
+  res.status(201).send({ id: payment.id })
 }
