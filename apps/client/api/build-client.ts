e68833faff1a56d8cd9cd0145286e6416874ded7
Igor Belogurov <belogurovigor@gmail.com>
2023-04-27 15:46:56 +0200

    - disable https; - replace baseUrl in build-client; - update prod/ingress-srv with DO fix

--- apps/client/api/build-client.ts
+++ apps/client/api/build-client.ts
@@ -3,11 +3,14 @@ import { NextPageContext } from 'next'
 
 export const buildClient = ({ req }: NextPageContext) => {
   if (typeof window === 'undefined') {
+    // We are on the server
     return axios.create({
-      baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      // baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      baseURL: 'http://www.getticket.store',
       headers: req?.headers,
     })
   } else {
+    // We must be on the browser
     return axios.create({
       baseURL: '/',
       headers: req?.headers,
